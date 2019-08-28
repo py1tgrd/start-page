4e28e0c3ebd8958591e4018c392c74d017abc7f3
py1tgrd <py1tgrd@googlemail.com>
2019-08-28 22:58:59 +0100

    Updates

---
+++ precache-manifest.04018b353a6c3c089ca7324c370b55af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2e5870715a7f7a7296e047fe68dc85",
+    "url": "/start-page/index.html"
+  },
+  {
+    "revision": "6840a2e55a75c9584d54",
+    "url": "/start-page/static/css/main.17c912c9.chunk.css"
+  },
+  {
+    "revision": "46b90f40ba3282cf7c56",
+    "url": "/start-page/static/js/2.12c011fc.chunk.js"
+  },
+  {
+    "revision": "6840a2e55a75c9584d54",
+    "url": "/start-page/static/js/main.779e2698.chunk.js"
+  },
+  {
+    "revision": "63651bb37b34291f1bde",
+    "url": "/start-page/static/js/runtime~main.184cd304.js"
+  }
+]);
